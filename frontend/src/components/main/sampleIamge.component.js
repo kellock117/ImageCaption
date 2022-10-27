6c487db60ce03798217be78e07c4b6b5f1f6593d
Tommy <jin31022324@gmail.com>
2022-10-27 21:14:58 +0800

    few changes

--- frontend/src/components/main/sampleIamge.component.js
+++ frontend/src/components/main/sampleIamge.component.js
@@ -4,24 +4,34 @@ import "./css/sampleImg.css";
 const sampleImgCont = {
   boxShadow: "0px 14px 80px rgba(34, 35, 58, 0.2)",
   borderRadius: "10px",
-  backgroundColor: 'white',
-  width: '480px',
+  backgroundColor: "white",
+  width: "480px",
   padding: "10px",
   position: "relative",
   top: "-40px",
-}
+};
 
 const sampleImgText = {
-  textAlign: 'left',
-  verticalAlign: 'text-top',
-}
+  textAlign: "left",
+  verticalAlign: "text-top",
+};
 
 export default function SampleImage({ onClick }) {
   return (
     <div style={sampleImgCont}>
       <p style={sampleImgText}>Sample Images : </p>
-      <img className="sampleimg1" alt="apple.jpg" src="./apple.jpg" onClick={onClick} />
-      <img className="sampleimg2" alt="tree.jpg" src="./tree.jpg" onClick={onClick} />
+      <img
+        className="sampleimg1"
+        alt="apple"
+        src="./apple.jpg"
+        onClick={onClick}
+      />
+      <img
+        className="sampleimg2"
+        alt="tree"
+        src="./tree.jpg"
+        onClick={onClick}
+      />
     </div>
   );
 }
